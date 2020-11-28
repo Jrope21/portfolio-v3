cbfa05eed13cb62b6424ed952a3657cc42fb86a2
jrope21 <joshbball8@gmail.com>
2020-11-28 12:30:55 -0600

    progress

--- src/components/home-components/numbered-section-title/numbered-section-title.component.jsx
+++ src/components/home-components/numbered-section-title/numbered-section-title.component.jsx
@@ -2,11 +2,22 @@ import React from 'react'
 import './numbered-section-title.styles.scss'
 
 import One from '@images/section-numbers/01.svg';
+import Two from '@images/section-numbers/02.svg';
+import Three from '@images/section-numbers/03.svg';
+import Four from '@images/section-numbers/04.svg';
 
-export default function NumberedSectionTitle({title, sectionNumber, className}) {
+
+const numberIcons = {
+    "1": One,
+    "2": Two,
+    "3": Three,
+    "4": Four
+}
+
+export default function NumberedSectionTitle({title, sectionNumber = "1", className}) {
     return (
         <div className={`numbered-section-title__module${className ? ` ${className}` : ''}`}>
-            <img src={One} alt="A number indicating the website section"/>
+            <img src={numberIcons[sectionNumber]} alt="A number indicating the website section"/>
             <h2 className="title">{title}</h2>
         </div>
     )
