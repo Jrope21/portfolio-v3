2ff8e1c3117bd3d49ef7c43f4580651c01e8e8e9
jrope21 <joshbball8@gmail.com>
2020-12-29 19:07:55 -0600

    animation progress

--- src/components/home-components/statement-and-skills/statement-and-skills.component.jsx
+++ src/components/home-components/statement-and-skills/statement-and-skills.component.jsx
@@ -6,12 +6,14 @@ import styles from '@styles/variables.scss'
 import HeroImageDesktop from '@images/home/about-me-desktop-img.jpg'
 import HeroImageMobile from '@images/home/about-me-mobile-img.jpg'
 import NumberedSectionTitle from '@home-components/numbered-section-title/numbered-section-title.component'
+import { skillsData } from './_data'
 
 const StatementAndSkills = React.forwardRef(({ sectionRef, activateAnimations = false }) => {
     return (
         <section 
             ref={sectionRef}
             className="statement-and-skills__module"
+            data-sal="mount"
         >
 
             <div className="relative">
@@ -20,19 +22,25 @@ const StatementAndSkills = React.forwardRef(({ sectionRef, activateAnimations = 
             <div className="top-container">
 
                 <NumberedSectionTitle
-                    className="title-sections"
+                    className="title-sections fade-up__mount"
                     title="Statement<br/> & Skills" 
                     sectionNumber="3" 
                     sectionName="about"
                 />
 
-                <p className="statement">
+                <p 
+                    className="statement fade-up__mount"
+                    style={{ transitionDelay: '100ms' }}
+                >
                     Hi there! I’m Josh, a Dallas-based <strong>front end developer</strong> with a knack for programming and design. My <strong>passion</strong> comes from being a part of a solution that brings each aspect of the customer journey together across both <strong>development and design</strong> processes. I focus on creating <strong>production-ready applications</strong> with my knowledge of the user experience, and writing scalable, <strong>clean code.</strong>
                 </p>
 
             </div>
 
-            <picture className="hero-image">
+            <picture 
+                className="hero-image fade-up__mount"
+                style={{ transitionDelay: '200ms' }}
+            >
                 <source 
                     srcSet={HeroImageDesktop}
                     media={`(min-width: ${styles.breakpointSm}px)`} 
@@ -41,14 +49,44 @@ const StatementAndSkills = React.forwardRef(({ sectionRef, activateAnimations = 
             </picture>
             </div>
 
-            <ul className="skills">
-                <li>React | Redux</li>
-                <li>Gatsby Js | React Static</li>
-                <li>Svelte 3 | Sapper</li>
-                <li>Node Js | Express</li>
-                <li>JavaScript (ES8+)</li>
-                <li>jQuery</li>
-                <li>Asnychronus JavaScript</li>
+            <ul 
+                className="skills list-animate__mount"    
+            >
+                {skillsData.map((skills, i) => (
+                    <li 
+                        key={'statement-skills-list' + skills}
+                        className="fade-up__mount"
+                        style={{ transitionDelay: `${450 + i * 50}ms` }}
+                    >{skills}</li>
+                ))}
+                {/* <li 
+                    className="fade-up__mount"
+                    style={{ transitionDelay: '450ms' }}
+                >React | Redux</li>
+                <li 
+                    className="fade-up__mount"
+                    style={{ transitionDelay: '500ms' }}
+                >Gatsby Js | React Static</li>
+                <li 
+                    className="fade-up__mount"
+                    style={{ transitionDelay: '500ms' }}
+                >Svelte 3 | Sapper</li>
+                <li 
+                    className="fade-up__mount"
+                    style={{ transitionDelay: '500ms' }}
+                >Node Js | Express</li>
+                <li 
+                    className="fade-up__mount"
+                    style={{ transitionDelay: '500ms' }}
+                >JavaScript (ES8+)</li>
+                <li 
+                    className="fade-up__mount"
+                    style={{ transitionDelay: '500ms' }}
+                >jQuery</li>
+                <li 
+                    className="fade-up__mount"
+                    style={{ transitionDelay: '500ms' }}
+                >Asnychronus JavaScript</li>
                 <li>WebPack</li>
                 <li>Git (Version Control)</li>
                 <li>Command Line</li>
@@ -63,7 +101,7 @@ const StatementAndSkills = React.forwardRef(({ sectionRef, activateAnimations = 
                 <li>Figma</li>
                 <li>SEO</li>
                 <li>Usability Testing</li>
-                <li>Agile | Waterfall</li>
+                <li>Agile | Waterfall</li> */}
             </ul>
 
         </section>
