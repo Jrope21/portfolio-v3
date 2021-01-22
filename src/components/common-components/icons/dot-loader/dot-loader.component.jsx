032ce862479829562a59e16c91e615e236976d92
jrope21 <joshbball8@gmail.com>
2021-01-21 21:57:08 -0600

    progress

--- src/components/common-components/icons/dot-loader/dot-loader.component.jsx
+++ src/components/common-components/icons/dot-loader/dot-loader.component.jsx
@@ -1,11 +1,24 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import "./dot-loader.styles.scss"
 
-const DotLoader = ({ overlay = false, loading = false }) => (
-  <div className={`dot-loader__module${loading ? ` loading` : ""}`}>
-    {overlay && <div className={"overlay"}></div>}
-    <div className={`loader`}>Loading...</div>
-  </div>
-)
+const DotLoader = ({ overlay = false, loading = false }) => {
 
+  const [showElement, setShowElement] = useState(false)
+  const [isFadeingOut, setIsFadeingOut] = useState(false)
+
+  useEffect(() => {
+    if(loading) setShowElement(true);
+    else setTimeout(() => {
+      setShowElement(false);
+    }, 250)
+  }, [loading])
+  
+  if(showElement) return (
+    <div className={`dot-loader__module${loading ? ` loading` : ""}`}>
+      {overlay && <div className={"overlay"}></div>}
+      <div className={`loader`}>Loading...</div>
+    </div>
+  )
+  return null;
+}
 export default DotLoader
--- src/components/global-components/footer/footer.component.jsx
+++ src/components/global-components/footer/footer.component.jsx
@@ -7,6 +7,9 @@ export default function Footer() {
     return (
         <footer className="footer__module container">
             <SocialIcons iconClassName={'footer-social-icon'} />
+            <a href="josh-resume.pdf" download className="download-btn">
+                Download Resume
+            </a>
             <p className="copyright">&copy;{new Date().getFullYear()} Joshua Roper Development</p>
         </footer>
     )
